fcd33fda5c2fb808fe6a296134d3f5514d30fcfa
max-shakhzod <buzzcodes@protonmail.com>
2024-10-06 22:40:51 +0800

    feat: "Adding the completed code files"

---
+++ src/components/CTA.jsx
@@ -0,0 +1,21 @@
+import styles from "../style";
+import Button from "./Button";
+
+const CTA = () => (
+  <section
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+  >
+    <div className="flex-1 flex flex-col">
+      <h2 className={styles.heading2}>Join Us Today</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Discover a futuristic solution that transforms payments into an easy, reliable, and secure experience.
+      </p>
+    </div>
+
+    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <Button />
+    </div>
+  </section>
+);
+
+export default CTA;
